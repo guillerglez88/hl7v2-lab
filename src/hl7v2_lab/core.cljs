d8934502adf6ed156c3d93ad3c59165458152a97
Guillermo <guillerglez88@gmail.com>
2024-04-22 22:49:28 +0200

    adding reitit

--- src/hl7v2_lab/core.cljs
+++ src/hl7v2_lab/core.cljs
@@ -1,12 +1,41 @@
 (ns hl7v2-lab.core
   (:require
-   [reagent.dom :as rd]))
+   [reagent.core :as r]
+   [reagent.dom.client :as rd]
+   [reitit.frontend :as rf]
+   [reitit.frontend.easy :as rfe]
+   [reitit.frontend.controllers :as rfc]
+   [clojure.pprint :as pp]))
 
-(defn app []
+(defonce match (r/atom nil))
+
+(defn home-page []
   [:div
    "Hi mom!"])
 
+(def router
+  (rf/router
+   ["/" {:name ::home
+         :view home-page}]))
+
+(defn app []
+  (if @match
+    (let [view (get-in @match [:data :view])]
+      [view @match])
+    [:pre
+     (with-out-str
+       (pp/pprint @match))]))
+
 (defn init []
+  (rfe/start!
+   router
+   (fn [new-match]
+     (when new-match
+       (reset! match
+               (if-let [cs (:controllers @match)]
+                 (assoc new-match :controllers (rfc/apply-controllers cs new-match))
+                 new-match))))
+   {:use-fragment true})
   (rd/render
-   [app]
-   (.getElementById js/document "app")))
+   (rd/create-root (.getElementById js/document "app"))
+   [app]))
