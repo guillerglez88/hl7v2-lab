447ecc35ec93b169a14d1d1d7ce659122cd15c71
Guillermo <guillerglez88@gmail.com>
2024-04-30 00:20:48 +0200

    fixing hot-reload

--- src/hl7v2_lab/core.cljs
+++ src/hl7v2_lab/core.cljs
@@ -4,40 +4,47 @@
    [reagent.dom.client :as rd]
    [reitit.frontend :as rf]
    [reitit.frontend.easy :as rfe]
-   [reitit.frontend.controllers :as rfc]
-   [clojure.pprint :as pp]))
+   [reitit.frontend.controllers :as rfc]))
+
+(defonce route-match (r/atom nil))
+(defonce root-component (r/atom nil))
+
+(defn app []
+  (if-let [view (get-in @route-match [:data :view])]
+    [view @route-match]
+    "not-found"))
 
 (defn home-page []
-  [:div
-   "Hi mom!"])
+  [:div.container
+   [:div.notification.is-primary
+    "Hi mom!"]])
 
 (def routes
   ["/" {:name ::home
-        :view home-page}])
+        :view (fn []
+                [home-page])}])
+
+(defn init-routes []
+  (rfe/start!
+   (rf/router routes)
+   (fn [match]
+     (when match
+       (reset! route-match
+               (if-let [cs (:controllers @route-match)]
+                 (assoc match :controllers (rfc/apply-controllers cs match))
+                 match))))
+   {:use-fragment true}))
+
+(defn ^:dev/after-load render-app []
+  (when-not @root-component
+    (reset! root-component (rd/create-root (.getElementById js/document "app"))))
+  (rd/render
+   @root-component
+   [app]))
 
 (defn init []
-  (let [route-match (r/atom nil)
-        init-router (fn []
-                      (rfe/start!
-                       (rf/router routes)
-                       (fn [match]
-                         (when match
-                           (reset! route-match
-                                   (if-let [cs (:controllers @route-match)]
-                                     (assoc match :controllers (rfc/apply-controllers cs match))
-                                     match))))
-                       {:use-fragment true}))
-        app (fn []
-              (if @route-match
-                (let [view (get-in @route-match [:data :view])]
-                  [view @route-match])
-                [:pre
-                 (with-out-str
-                   (pp/pprint @route-match))]))]
-    (init-router)
-    (when ^boolean goog.DEBUG
-      (enable-console-print!)
-      (println "--> dev mode"))
-    (rd/render
-     (rd/create-root (.getElementById js/document "app"))
-     [app])))
+  (when ^boolean goog.DEBUG
+    (enable-console-print!)
+    (println "--> dev mode"))
+  (init-routes)
+  (render-app))
